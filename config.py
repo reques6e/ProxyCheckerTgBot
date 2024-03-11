737def2efb10cbdb19c0008f1cac6a1a7d006aee
Alexander <149709871+reques6e@users.noreply.github.com>
2024-03-11 21:23:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-bot_token = '6706453403:AAH-2P7uc3mxHodL8AzQYUOR3frolnGoBJg'
+bot_token = ''
 
 crystal_pay_status = True
-crystal_pay_login = 'tetst231049872349023'
-crystal_pay_secret = '37e6f16b26019d4507d1612275357db4cec50acb'
-crystal_pay_salt = 'a17f502f97c6d430d7bbaf684c3fc337d830a338'
+crystal_pay_login = ''
+crystal_pay_secret = ''
+crystal_pay_salt = ''
